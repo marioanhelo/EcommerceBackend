572e96b55e3c38a4a3e5d853bcbc3476e70e5080
Mario Hernández <marioanhelo@hotmail.com>
2022-10-06 17:19:14 -0500

    Cambios en controllers

--- routes/index.js
+++ routes/index.js
@@ -27,44 +27,44 @@ const { createProduct,
 
 // CRUD de Products
 
-router.post('api/v1/products', createProduct)
-router.get('api/v1/products', getProducts)
-router.get('api/v1/products/:idProduct', getProduct)
-router.put('api/v1/products/:idProduct', modifyProduct)
-router.delete('api/v1/products/:idProduct', deleteProduct)
+router.post('/api/v1/products', createProduct)
+router.get('/api/v1/products', getProducts)
+router.get('/api/v1/products/:idProduct', getProduct)
+router.put('/api/v1/products/:idProduct', modifyProduct)
+router.delete('/api/v1/products/:idProduct', deleteProduct)
 
 
 
 // CRUD de Clients
 
-router.get('api/v1/clients', getClients)
-router.get('api/v1/clients/:idClient', getClient)
-router.put('api/v1/clients/:idClient', modifyClient)
-router.delete('api/v1/clients/:idClient', deleteClient)
+router.get('/api/v1/clients', getClients)
+router.get('/api/v1/clients/:idClient', getClient)
+router.put('/api/v1/clients/:idClient', modifyClient)
+router.delete('/api/v1/clients/:idClient', deleteClient)
 
 
 // CRUD de Invoices
 
-router.post('api/v1/invoices', createInvoice)
-router.get('api/v1/invoices', getInvoices)
-router.get('api/v1/invoices/:idInvoice', getInvoice)
-router.put('api/v1/invoices/:idInvoice', modifyInvoice)
-router.delete('api/v1/invoices/:idInvoice', deleteInvoice)
+router.post('/api/v1/invoices', createInvoice)
+router.get('/api/v1/invoices', getInvoices)
+router.get('/api/v1/invoices/:idInvoice', getInvoice)
+router.put('/api/v1/invoices/:idInvoice', modifyInvoice)
+router.delete('/api/v1/invoices/:idInvoice', deleteInvoice)
 
-router.post('api/v1/invoices/detail', addInvoiceDetail)
-router.get('api/v1/invoices/detail:idInvoice', getInvoiceDetail)
+router.post('/api/v1/invoices/detail', addInvoiceDetail)
+router.get('/api/v1/invoices/detail:idInvoice', getInvoiceDetail)
 
 // CRUD de Categorias
 
-router.post('api/v1/categories', createCategory)
-router.get('api/v1/categories', getCategories)
-router.get('api/v1/categories/:idInvoice', getCategory)
-router.put('api/v1/categories/:idInvoice', modifyCategory)
-router.delete('api/v1/categories/:idInvoice', deleteCategory)
+router.post('/api/v1/categories', createCategory)
+router.get('/api/v1/categories', getCategories)
+router.get('/api/v1/categories/:idInvoice', getCategory)
+router.put('/api/v1/categories/:idInvoice', modifyCategory)
+router.delete('/api/v1/categories/:idInvoice', deleteCategory)
 
 //LOGIN
 
-router.post('api/v1/login', loginController)
-router.post('api/v1/register', registerController)
+router.post('/api/v1/login', loginController)
+router.post('/api/v1/register', registerController)
 
 module.exports = router
